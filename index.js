dd6f1793473d0badd49e83a53ef93c0097c0894d
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-03-19 11:36:38 +1100

    Setup initial templating and home route

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const ejs = require('ejs');
+const express = require("express")
+const path = require("path")
+const morgan = require("morgan")
+const expressLayouts = require('express-ejs-layouts')
 
-const { port } = require("./config");
+const { port } = require("./config")
 
-const app = express();
+// Require routes
+const homeRouter = require("./routes/home.js")
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express()
 
-app.use("/static", express.static(path.join(__dirname, "public")));
-app.use(morgan("dev"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/static", express.static(path.join(__dirname, "public")))
+app.use(morgan("dev"))
+
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+
+// Use routers
+app.use("/", homeRouter)
+
 
 
 
 app.listen(port, () =>
   console.log(`Server is listening on http://localhost:${port}\n`)
-);
+)
---
+++ routes/home.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/database.js');
+
+router.get('/', (req, res) => {
+    res.render('pages/home')
+})
+
+module.exports = router;
