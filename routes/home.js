8869231b8e613db29765940dc5482f3b191a2d12
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-03-19 12:17:11 +1100

    Setup initial homepage input templating

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/database.js');
 
+
 router.get('/', (req, res) => {
-    res.render('pages/home')
+    db.any('SELECT * FROM tasks')
+    .then((tasks) => {
+        res.render('pages/home', {
+            tasks: tasks
+        })
+    })
+    .catch((err) => {
+        // error page
+    })    
 })
 
 module.exports = router;
