09cc0828c27bf92495cc4b0f8127182ef4bcc2b4
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-03-24 17:29:52 +1100

    Consider start of day when creating new tasks

--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,27 @@ const router = express.Router()
 const db = require('../db/database')
 
 router.get('/todaystasks', (req, res) => {
-  db.any("SELECT id, user_id, task, due_date, complete FROM tasks WHERE user_id = $1 AND due_date = CURRENT_DATE", req.session.userId)
-  .then((tasks) => {
-    res.json(tasks)
-  })
-  .catch((err) => {
-    console.log(err)
-    // TODO: error catch
-  })
+  // db.one("SELECT id, start_of_day FROM users WHERE id = $1", req.session.userId)
+  // .then((user) => {
+  //   let start_of_day = Number(user.start_of_day.slice(0, 2))
+
+    db.any("SELECT id, user_id, task, due_date, complete FROM tasks WHERE user_id = $1 AND due_date >= date_trunc('day', now()) AND due_date < (date_trunc('day', now()) + INTERVAL '1 day') ORDER BY complete ASC, task ASC", req.session.userId)
+    .then((tasks) => {
+      res.json(tasks)
+    })
+    .catch((err) => {
+      console.log(err)
+      // TODO: error catch
+    })
+  // })
+  // .catch((err) => {
+  //   console.log(err)
+  //   // TODO: error catch
+  // }) 
 })
 
 router.get('/alltasks', (req, res) => {
-  db.any("SELECT id, user_id, task, TO_CHAR(due_date, 'DD Mon') due_date, complete FROM tasks WHERE user_id = $1", req.session.userId)
+  db.any("SELECT id, user_id, task, TO_CHAR(due_date, 'DD Mon') due_date, complete FROM tasks WHERE user_id = $1 ORDER BY due_date ASC, complete ASC, task ASC", req.session.userId)
   .then((tasks) => {
     res.json(tasks)
   })
--- routes/home.js
+++ routes/home.js
@@ -7,15 +7,27 @@ router.get('/', (req, res) => {
 })
 
 router.post('/newtasktoday', (req, res) => {
-  db.none("INSERT INTO tasks(user_id, task, due_date, created_at) VALUES ($1, $2, CURRENT_DATE, now())", [req.session.userId, req.body.task])
-  .then(() => {
-    return res.end()
+  db.one("SELECT id, start_of_day FROM users WHERE id = $1", req.session.userId)
+  .then((user) => {
+    let hour = Number(user.start_of_day.slice(0, 2))
+    let minutes = Number(user.start_of_day.slice(3, 5))
+
+    db.none("INSERT INTO tasks(user_id, task, due_date, created_at) VALUES ($1, $2, now() - INTERVAL '$3 hours $4 minutes', now())", [req.session.userId, req.body.task, hour, minutes])
+    .then(() => {
+      return res.end()
+    })
+    .catch((err) => {
+      console.log(err)
+      res.render('pages/error', {
+        err: err
+      })
+    })
   })
   .catch((err) => {
     console.log(err)
-    res.render('pages/error', {
-      err: err
-    })
+      res.render('pages/error', {
+        err: err
+      })
   })
 })
 
